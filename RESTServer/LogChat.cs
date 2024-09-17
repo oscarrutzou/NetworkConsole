bc7c46753a5fd37a72069517d875b930d5cff249
Erik-DK20 <erpr2@hotmail.com>
2024-09-17 13:22:42 +0200

    Get/Post
    lal

---
+++ RESTServer/LogChat.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using RESTServer.ControllerClasses;
+using System.Diagnostics;
+
+namespace RESTServer
+{
+    
+    [ApiController]
+    [Route("[controller]")]
+    
+    public class LogChat : ControllerBase
+    {
+        private static string _log;
+        static LogChat()
+        {
+            
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_log);
+        }
+        
+        [HttpPost("{message}")]
+        public IActionResult Post(string message)
+        {
+            
+            return Ok(_log+message+"/n");
+        }
+
+        //string apiUrl = "https://localhost:7019/LogChat/";
+
+        //HttpClient httpClient = new HttpClient();
+        //HttpJsonPostData postData = new HttpJsonPostData()
+        //{
+            
+        //};
+    }
+}
